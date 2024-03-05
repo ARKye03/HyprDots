73102325ea81ef24fe6b471fdd0b0cb1578876fb
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-05 00:01:40 -0500

    Update reactivity, remove connections, export default, stack.items Optimized style.css Floating bar

--- ags/config.js
+++ ags/config.js
@@ -7,94 +7,49 @@ import App from "resource:///com/github/Aylur/ags/app.js";
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
 
-//#region Works2
-// Dynamic sway-like workspaces
-// function Workspaces() {
-//   const workspaces = Hyprland.bind("workspaces");
-//   const activeId = Hyprland.active.workspace.bind("id");
-
-//   const Wicons = [
-//     "",
-//     " ",
-//     " ",
-//     "󰨞 ",
-//     " ",
-//     " ",
-//     " ",
-//     " ",
-//     " ",
-//     "󰊖 ",
-//     " ",
-//   ];
-
-//   return Widget.Box({
-//     className: "workspaces",
-//     children: workspaces.as((ws) =>
-//       ws.map(({ id, windows }) => {
-//         let className = "bc"; // default value
-
-//         if (activeId[1] === id) {
-//           className = "focused";
-//         } else if (windows > 0) {
-//           className = "work";
-//         }
-
-//         return Widget.Button({
-//           onClicked: () => Hyprland.messageAsync(`dispatch workspace ${id}`),
-//           child: Widget.Label(`${Wicons[id]}`),
-//           className: className,
-//         });
-//       })
-//     ),
-//   });
-// }
-//#endregion
-
 const Workspaces = () =>
   Widget.Box({
     className: "workspaces",
-    // @ts-ignore
-    connections: [
-      [
-        Hyprland.active.workspace,
-        (
-          /** @type {{ children: import("./types/widgets/button").Button<import("./types/widgets/label").Label<unknown>, unknown>[]; }} */ self
-        ) => {
-          const Wicons = [
-            "",
-            " ",
-            " ",
-            "󰨞 ",
-            " ",
-            " ",
-            " ",
-            " ",
-            " ",
-            "󰊖 ",
-            "",
-          ];
-          const arr = Array.from({ length: 10 }, (_, i) => i + 1);
-          self.children = arr.map((i) => {
-            let className = "bc"; // default value
+    setup: (self) => {
+      const Wicons = [
+        "",
+        " ",
+        " ",
+        "󰨞 ",
+        " ",
+        " ",
+        " ",
+        " ",
+        " ",
+        "󰊖 ",
+        "",
+      ];
+      const arr = Array.from({ length: 10 }, (_, i) => i + 1);
 
-            if (Hyprland.active.workspace.id === i) {
-              className = "focused";
-            } else if (
-              Hyprland.workspaces.some((ws) => ws.id === i && ws.windows > 0)
-            ) {
-              className = "work";
-            }
+      const updateWorkspaces = () => {
+        self.children = arr.map((i) => {
+          let className = ""; // default value
 
-            return Widget.Button({
-              onClicked: () =>
-                execAsync(`/usr/bin/hyprctl dispatch workspace ${i}`),
-              child: Widget.Label(`${Wicons[i]}`),
-              className: className,
-            });
+          if (Hyprland.active.workspace.id === i) {
+            className = "focused";
+          } else if (
+            Hyprland.workspaces.some((ws) => ws.id === i && ws.windows > 0)
+          ) {
+            className = "work";
+          }
+
+          return Widget.Button({
+            onClicked: () =>
+              execAsync(`/usr/bin/hyprctl dispatch workspace ${i}`),
+            child: Widget.Label(`${Wicons[i]}`),
+            className: className,
           });
-        },
-      ],
-    ],
+        });
+      };
+
+      self.hook(Hyprland.active.workspace, updateWorkspaces, "changed");
+      updateWorkspaces();
+    },
   });
 
 const date = Variable("", {
@@ -125,23 +80,25 @@ function Notification() {
 const MusicPlayerDaemon = Widget.Button({
   className: "media",
   child: Widget.Label({
-    // @ts-ignore
-    connections: [
-      [
-        1000,
-        (/** @type {{ label: string; }} */ self) =>
-          execAsync([
-            "/usr/bin/mpc",
-            "--format",
-            "[%artist% - %title%]",
-            "current",
-          ])
-            .then((date) => (self.label = date))
-            .catch(console.error),
-      ],
-    ],
+    setup: (self) => {
+      const updateLabel = () => {
+        execAsync([
+          "/usr/bin/mpc",
+          "--format",
+          "[%title% - %artist%]",
+          "current",
+        ])
+          .then((date) => {
+            self.label = date;
+            // Wait for player status change and then update the label again
+            execAsync(["mpc", "idle", "player"]).then(updateLabel);
+          })
+          .catch(console.error);
+      };
+      updateLabel();
+    },
   }),
-  on_primary_click: () =>
+  on_primary_click_release: () =>
     execAsync(
       `/usr/bin/alacritty --class floatcritty -e /usr/bin/ncmpcpp -c /home/archkye/.ncmpcpp/config`
     ),
@@ -149,14 +106,14 @@ const MusicPlayerDaemon = Widget.Button({
 
 const Rofi = Widget.Button({
   className: "bc",
-  child: Widget.Label("⏻"),
-  on_primary_click: () =>
+  child: Widget.Label(" ⏻ "),
+  on_primary_click_release: () =>
     execAsync(`/home/archkye/.config/rofi/powermenu/type-4/powermenu.sh`),
 });
 const Power = Widget.Button({
   className: "bc",
-  child: Widget.Label("󱓞"),
-  on_primary_click: () =>
+  child: Widget.Label(" 󱓞 "),
+  on_primary_click_release: () =>
     execAsync(`/home/archkye/.config/rofi/launchers/type-3/launcher.sh`),
 });
 
@@ -168,77 +125,52 @@ const Volume = () =>
     onScrollDown: () =>
       execAsync(`/usr/bin/wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-`),
     child: Widget.Box({
-      children: [
-        Widget.Stack({
-          items: [
-            // tuples of [string, Widget]
-            ["101", Widget.Icon("audio-volume-overamplified-symbolic")],
-            ["67", Widget.Icon("audio-volume-high-symbolic")],
-            ["34", Widget.Icon("audio-volume-medium-symbolic")],
-            ["1", Widget.Icon("audio-volume-low-symbolic")],
-            ["0", Widget.Icon("audio-volume-muted-symbolic")],
-          ],
-          // @ts-ignore
-          connections: [
-            [
-              Audio,
-              (/** @type {{ shown: string; }} */ self) => {
-                if (!Audio.speaker) return;
+      setup: (self) => {
+        const updateVolume = () => {
+          const volumeIcon = Widget.Icon().hook(Audio.speaker, (self) => {
+            const category = {
+              101: "overamplified",
+              67: "high",
+              34: "medium",
+              1: "low",
+              0: "muted",
+            };
 
-                if (Audio.speaker.is_muted) {
-                  self.shown = "0";
-                  return;
-                }
-
-                const show = [101, 67, 34, 1, 0].find(
+            const icon = Audio.speaker.is_muted
+              ? 0
+              : [101, 67, 34, 1, 0].find(
                   (threshold) => threshold <= Audio.speaker.volume * 100
                 );
 
-                self.shown = `${show}`;
-              },
-              "speaker-changed",
-            ],
-          ],
-        }),
-        Widget.Label({
-          // @ts-ignore
-          connections: [
-            [
-              Audio,
-              (/** @type {{ label: string; }} */ self) => {
-                self.label = `   ${Math.round(Audio.speaker.volume * 100)}`;
-              },
-              "speaker-changed",
-            ],
-          ],
-        }),
-      ],
+            self.icon = `audio-volume-${category[icon]}-symbolic`;
+          });
+
+          const volumeLabel = Widget.Label({
+            label: `   ${Math.round(Audio.speaker.volume * 100)}`,
+          });
+
+          self.children = [volumeIcon, volumeLabel];
+        };
+
+        self.hook(Audio, updateVolume, "speaker-changed");
+        updateVolume();
+      },
     }),
   });
 
 const SysTray = () =>
   Widget.Box({
     className: "systray",
-    // @ts-ignore
-    connections: [
-      [
-        SystemTray,
-        (
-          /** @type {{ children: import("./types/widgets/button").Button<import("./types/widgets/icon").Icon<unknown>, unknown>[]; }} */ self
-        ) => {
-          self.children = SystemTray.items.map((item) =>
-            Widget.Button({
-              // @ts-ignore
-              child: Widget.Icon({ binds: [["icon", item, "icon"]] }),
-              onPrimaryClick: (_, event) => item.activate(event),
-              onSecondaryClick: (_, event) => item.openMenu(event),
-              // @ts-ignore
-              binds: [["tooltip-markup", item, "tooltip-markup"]],
-            })
-          );
-        },
-      ],
-    ],
+    children: SystemTray.bind("items").as((items) =>
+      items.map((item) =>
+        Widget.Button({
+          child: Widget.Icon({ icon: item.bind("icon") }),
+          on_primary_click: (_, event) => item.activate(event),
+          on_secondary_click: (_, event) => item.openMenu(event),
+          tooltip_markup: item.bind("tooltip_markup"),
+        })
+      )
+    ),
   });
 
 // layout of the bar
@@ -258,29 +190,21 @@ const Right = () =>
     children: [Volume(), Clock(), SysTray(), Rofi],
   });
 
-// @ts-ignore
-const Bar = ({ monitor } = {}) =>
+const Bar = (monitor = 0) =>
   Widget.Window({
     name: `bar-${monitor}`, // name has to be unique
-    className: "bar",
+    class_name: "bar",
     monitor,
     anchor: ["top", "left", "right"],
     exclusivity: "exclusive",
     child: Widget.CenterBox({
-      startWidget: Left(),
-      centerWidget: Center(),
-      endWidget: Right(),
+      start_widget: Left(),
+      center_widget: Center(),
+      end_widget: Right(),
     }),
   });
 
-// exporting the config so ags can manage the windows
-export default {
-  style: App.configDir + "/style.css",
-  windows: [
-    Bar(),
-
-    // you can call it, for each monitor
-    // Bar({ monitor: 0 }),
-    // Bar({ monitor: 1 })
-  ],
-};
+App.config({
+  style: "./style.css",
+  windows: [Bar()],
+});
