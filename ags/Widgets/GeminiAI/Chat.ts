2fe2fa481f04c94cfb8acf497f15a650c3afe42c
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-07 00:51:51 -0500

    WIP Chat

--- ags/Widgets/GeminiAI/Chat.ts
+++ ags/Widgets/GeminiAI/Chat.ts
@@ -1,11 +1,42 @@
 export const Chat = () =>
   Widget.Window({
     name: "Chat",
+    keymode: "on-demand",
+    width_request: 400,
     visible: false,
     class_name: "chat",
     anchor: ["top", "right", "bottom"],
-    child: Widget.Button({
-      on_primary_click_release: () => console.log("Hello"),
-      child: Widget.Label("HOLAAAAAAAAAAA"),
+    child: Widget.CenterBox({
+      vertical: true,
+      startWidget: Widget.Box(
+        Widget.Label({
+          label: "Gemini here for you.",
+          justification: "left",
+        }),
+        Widget.Icon({
+          icon: "/home/archkye/.config/ags/Widgets/GeminiAI/pacman.svg",
+          class_name: "pacman",
+          size: 60,
+        })
+      ),
+      centerWidget: Widget.Label({
+        css: "background-color: #3B192F",
+        label: "MOREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEe",
+        height_request: 900,
+      }),
+      endWidget: Widget.Box({
+        height_request: 40,
+        spacing: 8,
+        homogeneous: false,
+        vertical: false,
+        children: [
+          Widget.Entry({
+            placeholder_text: "Ask Gemini...",
+            visibility: true, // set to false for passwords
+            onAccept: ({ text }) => print(text),
+          }),
+          Widget.Button(),
+        ],
+      }),
     }),
   });
