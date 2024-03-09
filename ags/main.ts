a32b4733dc45ff0c52e60b23d5b4dc10aa6a32bb
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-09 11:55:55 -0500

    Add [Comment] import statement for Dock widget

--- ags/main.ts
+++ ags/main.ts
@@ -2,8 +2,9 @@ import App from "resource:///com/github/Aylur/ags/app.js";
 import { notificationPopup } from "./Widgets/NotificationPops";
 import { Chat } from "Widgets/GeminiAI/Chat";
 import { Bar } from "Widgets/Bar";
+//import Dock from "Widgets/Dock/main";
 
 App.config({
   style: "./styles/style.css",
-  windows: [Bar(), notificationPopup, Chat()],
+  windows: [Bar(), notificationPopup, Chat() /* , Dock() */],
 });
