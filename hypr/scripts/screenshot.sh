353faabdb5019c7dc5a38b2e9caa52c5c8450bef
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-16 16:28:47 -0400

    Remove screenshot.sh script

--- hypr/scripts/screenshot.sh
+++
@@ -1,6 +0,0 @@
-gscreenshot -f ./Screenshots
-if [ $? -eq 0 ]; then
-    hyprctl notify 1 2000 "rgb(00ce71)" "Screenshot taken"
-else
-    hyprctl notify 5 10000 "rgb(ce0000)" "Failed to take the screenshot"
-fi
