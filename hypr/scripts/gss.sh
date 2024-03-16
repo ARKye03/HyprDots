5d65b85ac9f98ad9b56e381a666a341d6b9b9462
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-16 16:29:05 -0400

    Add keybindings for different types of screenshots***

---
+++ hypr/scripts/gss.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function region_screenshot {
+  grim -g "$(slurp)" - | swappy -f -
+}
+
+function file_screenshot {
+  swappy -f "~/Desktop/my-gnome-saved-file.png"
+}
+
+function stdout_screenshot {
+  grim -g "$(slurp)" - | swappy -f - -o - | pngquant -
+}
+
+function window_screenshot {
+  grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)" - | swappy -f -
+}
+
+function full_screenshot {
+  grim
+  if [ $? -eq 0 ]; then
+    notify-send -u low -t 2000 "Screenshot stored in ~/Pictures"
+  fi
+}
+
+function custom_slurp {
+  slurp -d -c \#ffffff -b \#00000000
+}
+
+case "$1" in
+  region)
+    region_screenshot
+  ;;
+  file)
+    file_screenshot
+  ;;
+  stdout)
+    stdout_screenshot
+  ;;
+  window)
+    window_screenshot
+  ;;
+  full)
+    full_screenshot
+  ;;
+  aspect_visualizer)
+    custom_slurp
+  ;;
+  *)
+    echo "Invalid argument. Please use one of the following: region, file, stdout, window, full, custom"
+  ;;
+esac
