ccc97c04e4c4a9721e6bac987ccdf88acc9d8396
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-18 15:15:29 -0400

    Update date format in Clock widget

--- ags/Widgets/Bar/Clock.ts
+++ ags/Widgets/Bar/Clock.ts
@@ -1,7 +1,7 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 
 const date = Variable("", {
-  poll: [60000, 'date "+%H:%M %b %e"'],
+  poll: [60000, 'date "+%I:%M %p %b %e"'],
 });
 export const Clock = () =>
   Widget.Label({
