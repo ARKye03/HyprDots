5295600786bce624d7b63d4144d0435ddf6e510e
ARKye03 <rafa2003sanchez@gmail.com>
2024-03-26 16:22:15 -0400

    Revert "New SideChatBar UI"
    This reverts commit 937215a2554fcb51817be2bdd3bd2e1b7836605b.

--- ags/Widgets/GeminiAI/Chat.ts
+++ ags/Widgets/GeminiAI/Chat.ts
@@ -1,25 +1,146 @@
 // @ts-ignore
-import WebKit2 from "gi://WebKit2";
-import GLib from "gi://GLib";
+import Gtk from "gi://Gtk";
+import { fetchCode } from "./GeminiAPI";
+import { icons } from "assets/Assets";
 
-// Create a WebView
-const webView = new WebKit2.WebView();
+const TextEntryWidget = Widget.subclass(Gtk.TextView);
 
-// Convert the path of your HTML file to a file:// URI
-const htmlFilePath = GLib.filename_to_uri("/home/archkye/.config/ags/Widgets/GeminiAI/GeminiChat.html", null);
-console.log(htmlFilePath);
+const TextInputWidget = TextEntryWidget({
+  // @ts-ignore
+  wrap_mode: Gtk.WrapMode.WORD_CHAR,
+  left_margin: 5,
+  top_margin: 5,
+  setup(self) {
+    self.bind();
+  },
+});
 
-// Load the HTML file into the WebView
-webView.load_uri(htmlFilePath);
+// Wrap the TextInputWidget in a Scrollable
+const ScrollableTextInputWidget = Widget.Scrollable({
+  vscroll: "automatic",
+  hscroll: "never",
+  class_name: "inputBox",
+  child: TextInputWidget,
+});
+// Create a reference to the center ListBox
+const CenterListBox = Widget.ListBox({
+  hexpand: false,
+  class_name: "chatBox",
+  setup(self) {
+    self.add(
+      Widget.Label({
+        wrap: true,
+        use_markup: true,
+        label: "Hi there! How can I help you today?",
+      })
+    );
+  },
+});
+const geminiChatHeader = Widget.Box({
+  vexpand: true,
+  class_name: "chatHeader",
+  children: [
+    Widget.Icon({
+      icon: icons.Pacman,
+      class_name: "pacman",
+      size: 60,
+      hpack: "start",
+      hexpand: true,
+    }),
+    Widget.Label({
+      label: "Gemini here for you.",
+      hpack: "center",
+      hexpand: true,
+    }),
+    Widget.Button({
+      can_focus: false,
+      onClicked: () => App.toggleWindow("Chat"),
+      child: Widget.Icon({
+        icon: icons.closeChatSvg,
+        size: 50,
+        hexpand: true,
+      }),
+    }),
+  ],
+});
+const ScrollableListBox = Widget.Scrollable({
+  class_name: "scrollChatBox",
+  hscroll: "never",
+  vscroll: "automatic",
+  child: CenterListBox,
+});
+const resultListBox = Widget.Box({
+  class_name: "input",
+  spacing: 8,
+  vexpand: true,
+  homogeneous: false,
+  vertical: false,
+  children: [
+    ScrollableTextInputWidget,
+    Widget.Button({
+      can_focus: false,
+      class_name: "sendButton",
+      onClicked: async () => {
+        let startIter = TextInputWidget.get_buffer().get_start_iter();
+        let endIter = TextInputWidget.get_buffer().get_end_iter();
+        let textToSend = TextInputWidget.get_buffer().get_text(
+          startIter,
+          endIter,
+          true
+        );
+        print("Send: " + textToSend);
+        fetchCode(textToSend)
+          .then((result) => {
+            console.log(result);
+            let resultText = Array.isArray(result) ? result.join(" ") : result;
 
+            // Create a new Box with a Label containing the text
+            let row = Widget.Box();
+            let label = Widget.Label({
+              label: resultText,
+              wrap: true,
+              use_markup: true,
+              selectable: true,
+            });
+            row.add(label);
+
+            // Add the Box to the top of the ListBox
+            CenterListBox.prepend(row);
+
+            // Show the Box
+            row.show_all();
+          })
+          .catch((error) => {
+            // handle the error here
+            console.log(error);
+          });
+      },
+      child: Widget.Icon({
+        icon: icons.sendSvg,
+        size: 20,
+      }),
+    }),
+  ],
+});
+
+const sideBarContent = Widget.CenterBox({
+  vertical: true,
+  vexpand: true,
+  startWidget: geminiChatHeader,
+  centerWidget: ScrollableListBox,
+  endWidget: resultListBox,
+});
 export const Chat = () =>
   Widget.Window({
     name: "Chat",
-    width_request: 400,
     keymode: "on-demand",
     class_name: "chat",
     exclusivity: "normal",
     visible: false,
+<<<<<<< HEAD
+=======
+    class_name: "chat",
+>>>>>>> parent of 937215a (New SideChatBar UI)
     anchor: ["top", "left", "bottom"],
-    child: webView,
+    child: sideBarContent,
   });
