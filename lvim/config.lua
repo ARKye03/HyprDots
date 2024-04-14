893a56dee0d23c3a32a71fcc9142e9e91495b1c8
ARKye03 <rafa2003sanchez@gmail.com>
2024-04-14 15:14:38 -0400

    Add onedark plugin and relative numbers to lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,9 +2,17 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.transparent_window = true
 lvim.keys.normal_mode["<A-Tab>"] = ":bn<CR>"
+lvim.format_on_save = true
 
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
 vim.opt.clipboard = "unnamedplus"
+vim.opt.relativenumber = true
+
+-- Lazy
+lvim.plugins = {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 100,
+  },
+}
+lvim.colorscheme = "onedark_vivid"
