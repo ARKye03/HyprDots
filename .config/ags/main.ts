c93a6ae4ffbc9d5aff11bdec85b82e943ba03b0e
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-17 21:27:48 -0400

    Formatted code for no particular reason

--- .config/ags/main.ts
+++ .config/ags/main.ts
@@ -1,11 +1,11 @@
+import { applauncher } from "Widgets/AppLauncher";
+import { Bar } from "Widgets/Bar/Bar";
+import { calendar } from "Widgets/Bar/Clock";
+import { Chat } from "Widgets/GeminiAI/Chat";
+import { MediaWidget } from "Widgets/Media/Media";
+import { notificationSideBar } from "Widgets/Notifications/NotificationCenter";
 import App from "resource:///com/github/Aylur/ags/app.js";
 import { notificationPopup } from "./Widgets/Notifications/NotificationPops";
-import { notificationSideBar } from "Widgets/Notifications/NotificationCenter";
-import { Chat } from "Widgets/GeminiAI/Chat";
-import { Bar } from "Widgets/Bar/Bar";
-import { MediaWidget } from "Widgets/Media/Media";
-import { applauncher } from "Widgets/AppLauncher";
-import { calendar } from "Widgets/Bar/Clock";
 
 // main scss file
 const scss = `${App.configDir}/styles/MainStyle.scss`;
@@ -29,7 +29,7 @@ Utils.monitorFile(
     Utils.exec(`sassc ${scss} ${css}`);
     App.resetCss();
     App.applyCss(css);
-  }
+  },
 );
 // make sure sassc is installed on your system
 Utils.exec(`sassc ${scss} ${css}`);
