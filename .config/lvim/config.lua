226f64e7a018a7f4bc3c723e2f8cd00baea29c83
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-17 21:31:58 -0400

    One line less according to the git diff, yikes

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -2,7 +2,6 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-lvim.keys.normal_mode["<A-Tab>"] = ":bn<CR>"
 lvim.format_on_save = true
 
 vim.opt.clipboard = "unnamedplus"
