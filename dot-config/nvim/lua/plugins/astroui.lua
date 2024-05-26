6a6f1777749a5ad031c5c7e5f9a2d2ae55acfbf3
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-26 15:15:31 -0400

    AstroNvim now transparent bg

--- dot-config/nvim/lua/plugins/astroui.lua
+++ dot-config/nvim/lua/plugins/astroui.lua
@@ -1,9 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
+-- if true then return {} end
 
 ---@type LazySpec
 return {
@@ -15,7 +10,7 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+        Normal = { guibg = NONE },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
