3e534e52bb70c27030d5d98c1e25dc7f46b11004
ARKye03 <rafa2003sanchez@gmail.com>
2024-05-29 21:23:48 -0400

    update sh

--- scripts/toggle_vpn.sh
+++ scripts/toggle_vpn.sh
@@ -5,23 +5,24 @@
 
 # Function to connect to ProtonVPN
 connect_vpn() {
-  notify-send -u low -i dialog-information "Connecting Proton VPN"
-  protonvpn-cli c --cc US -p udp
-  if [ $? -eq 0 ]; then
-    # Get the server information
-    vpn_status=$(protonvpn-cli status)
-    server_info=$(echo "$vpn_status" | grep "Server:" | awk '{print $2}')
-    notify-send -u low -i dialog-information "ProtonVPN Connected:" "$server_info"
-  else
-    # If connection fails, prompt the user with Retry and Dismiss options
-    notify-send -u critical -i dialog-error "Failed to connect" -A "Retry" -A "Dismiss"
-    # Capture the exit status of notify-send
-    response=$?
-    if [ $response -eq 0 ]; then
-      # If Retry is selected, reconnect
-      connect_vpn
-    fi
-  fi
+	notify-send -u low -i dialog-information "Connecting Proton VPN"
+	protonvpn-cli c --cc US -p udp
+	if [ $? -eq 0 ]; then
+		# Get the server information
+		vpn_status=$(protonvpn-cli status)
+		server_info=$(echo "$vpn_status" | grep "Server:" | awk '{print $2}')
+		notify-send -u low -i dialog-information "ProtonVPN Connected:" "$server_info"
+	else
+		# If connection fails, prompt the user with Retry and Dismiss options
+		notify-send -u critical -i dialog-error "Failed to connect" -A "Retry" -A "Dismiss"
+		# Capture the exit status of notify-send
+		response=$?
+		notify-send -u low "$response"
+		if [ $response -eq 0 ]; then
+			# If Retry is selected, reconnect
+			connect_vpn
+		fi
+	fi
 }
 
 # Check the status of the VPN
@@ -29,11 +30,11 @@ vpn_status=$(protonvpn-cli status)
 
 # Check if the VPN is connected
 if echo "$vpn_status" | grep -q "Country"; then
-  # If the VPN is connected, disconnect it
-  notify-send -u critical -i dialog-warning "Disconnecting Proton VPN"
-  protonvpn-cli d
-  notify-send -u normal -i dialog-information "Disconnected"
+	# If the VPN is connected, disconnect it
+	notify-send -u critical -i dialog-warning "Disconnecting Proton VPN"
+	protonvpn-cli d
+	notify-send -u normal -i dialog-information "Disconnected"
 else
-  # If the VPN is not connected, call the connect function
-  connect_vpn
+	# If the VPN is not connected, call the connect function
+	connect_vpn
 fi
