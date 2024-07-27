7b6e4725f3eff56008d70bc58982c1ad007a0392
ARKye03 <rafa2003sanchez@gmail.com>
2024-07-26 21:17:31 -0400

    chore: Update utils.lua with improved key bindings and tag management

--- dot-config/river/utils.lua
+++ dot-config/river/utils.lua
@@ -1,6 +1,3 @@
--- riverctl map normal Super Tab focus-previous-tags
--- riverctl map normal Super Return spawn /usr/bin/alacritty
-
 local Mapping = {}
 
 function Mapping.Spawn(modkeys, keybind, command)
@@ -21,6 +18,17 @@ function Mapping.Action(modkeys, keybind, option)
     os.execute(cmd)
 end
 
+function Mapping.Pointer(modkeys, keybind, option)
+    local modkeys_str = table.concat(modkeys, "+")
+    local cmd = string.format("riverctl map-pointer normal %s %s %s", modkeys_str, keybind, option)
+    os.execute(cmd)
+end
+
+function Mapping.Property(property, value)
+    local cmd = string.format("riverctl %s %s", property, value)
+    os.execute(cmd)
+end
+
 local tags = {
     [1] = 1,
     [2] = 2,
@@ -35,7 +43,7 @@ local tags = {
 }
 
 function Mapping.ViewRule(id, tag)
-    local cmd = string.format("riverctl rule-add -app-id %s tags $s", id, tags[tag])
+    local cmd = string.format("riverctl rule-add -app-id %s tags %s", id, tags[tag])
     os.execute(cmd)
 end
 
@@ -45,26 +53,30 @@ function Mapping.ViewSSD(id)
 end
 
 function Mapping.SetFocusedTag(modkeys, keybind, tag)
+    local modkeys_str = table.concat(modkeys, "+")
     local cmd = string.format(
-        "riverctl map normal %s %s set-focused-tags %s", modkeys, keybind, tags[tag])
+        "riverctl map normal %s %s set-focused-tags %s", modkeys_str, keybind, tags[tag])
     os.execute(cmd)
 end
 
 function Mapping.SetViewTag(modkeys, keybind, tag)
+    local modkeys_str = table.concat(modkeys, "+")
     local cmd = string.format(
-        "riverctl map normal %s %s set-view-tags %s", modkeys, keybind, tags[tag])
+        "riverctl map normal %s %s set-view-tags %s", modkeys_str, keybind, tags[tag])
     os.execute(cmd)
 end
 
 function Mapping.ToggleFocusedTag(modkeys, keybind, tag)
+    local modkeys_str = table.concat(modkeys, "+")
     local cmd = string.format(
-        "riverctl map normal %s %s toggle-focused-tags %s", modkeys, keybind, tags[tag])
+        "riverctl map normal %s %s toggle-focused-tags %s", modkeys_str, keybind, tags[tag])
     os.execute(cmd)
 end
 
 function Mapping.ToggleViewTag(modkeys, keybind, tag)
+    local modkeys_str = table.concat(modkeys, "+")
     local cmd = string.format(
-        "riverctl map normal %s %s toggle-view-tags %s", modkeys, keybind, tags[tag])
+        "riverctl map normal %s %s toggle-view-tags %s", modkeys_str, keybind, tags[tag])
     os.execute(cmd)
 end
 
