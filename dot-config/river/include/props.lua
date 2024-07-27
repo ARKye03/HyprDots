17416c012fce0dda23e2582d34162a5416a64c8e
ARKye03 <rafa2003sanchez@gmail.com>
2024-07-27 13:54:58 -0400

    Refactor code, add focus cursor

--- dot-config/river/include/props.lua
+++ dot-config/river/include/props.lua
@@ -7,18 +7,16 @@ function Props()
         border_color_focused = "0x8aadf4",
         border_color_unfocused = "0x586e75",
 
+        focus_follows_cursor = "normal",
         -- Keyboard
         set_repeat = "50 300",
 
-        -- Layout
         default_layout = "rivertile",
     }
 
-    Utils.Property("background-color", opts.background_color)
-    Utils.Property("border-color-focused", opts.border_color_focused)
-    Utils.Property("border-color-unfocused", opts.border_color_unfocused)
-    Utils.Property("set-repeat", opts.set_repeat)
-    Utils.Property("default-layout", opts.default_layout)
+    for prop, value in pairs(opts) do
+        Utils.Property(prop, value)
+    end
 end
 
 return Props
