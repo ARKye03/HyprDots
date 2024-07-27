220fcfa6634e7a70a19a0c6cd90b52ddbf6a6274
ARKye03 <rafa2003sanchez@gmail.com>
2024-07-27 13:19:15 -0400

    Trying to set up xeus-lua fuck

--- dot-config/river/test/test.lua
+++ dot-config/river/test/test.lua
@@ -1,5 +1,9 @@
 local Utils = require('utils')
 
-Utils.Spawn({ "Super" }, "Return", "/usr/bin/alacritty")
-Utils.Spawn({ "Super" }, "M",
-    "/usr/bin/alacritty --class floatcritty -e /usr/bin/ncmpcpp -c " .. os.getenv("HOME") .. "/.ncmpcpp/config")
+-- Utils.Spawn({ "Super" }, "Return", "/usr/bin/alacritty")
+-- Utils.Spawn({ "Super" }, "M",
+--     "/usr/bin/alacritty --class floatcritty -e /usr/bin/ncmpcpp -c " .. os.getenv("HOME") .. "/.ncmpcpp/config")
+
+Utils.Spawn({ "Super" }, "V", os.getenv("HOME") .. "/.dotfiles/scripts/toggle_vpn.lua")
+
+Utils.Action({ "Super" }, "Space", "toggle-float")
