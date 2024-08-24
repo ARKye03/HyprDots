7fa2428ac90da8a38936d1dc4e15b1771ce62080
ARKye03 <rafa03-dev@proton.me>
2024-08-24 09:49:11 -0400

    chore: Update Hyprlang LSP and tree-sitter configuration

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -18,8 +18,8 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
--- Hyprlang LSP
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+-- Hyprlang LSP and tree-sitter
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
   pattern = { "*.hl", "hypr*.conf" },
   callback = function(event)
     print(string.format("starting hyprls for %s", vim.inspect(event)))
@@ -28,7 +28,10 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
       cmd = { "hyprls" },
       root_dir = vim.fn.getcwd(),
     }
-  end,
+    vim.filetype.add({
+      pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+    })
+  end
 })
 
 require "lazy_setup"
